edca36dc9f98d1e22b013ebf34c6feaf89ce4e05
Kevin Wainwright <kevin.wainwright@gmail.com>
2015-12-18 16:19:08 -0500

    Additional Activities

--- app/src/main/java/com/mse/android/javathehutt/JavaTheHutt.java
+++ app/src/main/java/com/mse/android/javathehutt/JavaTheHutt.java
@@ -1,5 +1,7 @@
 package com.mse.android.javathehutt;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,13 +10,30 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
 
-public class JavaTheHutt extends AppCompatActivity {
+import java.util.Map;
+
+public class JavaTheHutt extends AppCompatActivity implements View.OnClickListener {
+
+    private ViewGroup mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_java_the_hutt);
+
+        mListView = (ViewGroup) findViewById(R.id.list);
+
+        addDemo("Login", LoginActivity.class);
+        //addDemo("Plan Route", MapActivity.class);
+        addDemo("Begin Trip", BeginTripActivity.class);
+        addDemo("Collect Data", CollectDataActivity.class);
+        addDemo("Complete Trip", CompleteTripActivity.class);
+        addDemo("Monitor Trip", MonitorTripActivity.class);
+        addDemo("Analyze Trips", AnalTripsActivity.class);
+
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
@@ -26,6 +45,22 @@ public class JavaTheHutt extends AppCompatActivity {
              //           .setAction("Action", null).show();
            // }
         //});
+    }
+
+    private void addDemo(String demoName, Class<? extends Activity> activityClass) {
+        Button b = new Button(this);
+        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        b.setLayoutParams(layoutParams);
+        b.setText(demoName);
+        b.setTag(activityClass);
+        b.setOnClickListener(this);
+        mListView.addView(b);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Class activityClass = (Class) view.getTag();
+        startActivity(new Intent(this, activityClass));
     }
 
     @Override
