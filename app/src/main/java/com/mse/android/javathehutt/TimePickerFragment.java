eaaf9d9f0e053b8b0ce48b4e9e5f340ff2ff02f1
Kevin Wainwright <kevin.wainwright@gmail.com>
2015-12-18 22:34:38 -0500

    Initial Activity 4

---
+++ app/src/main/java/com/mse/android/javathehutt/TimePickerFragment.java
@@ -0,0 +1,39 @@
+package com.mse.android.javathehutt;
+
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TimePickerFragment extends DialogFragment
+    implements TimePickerDialog.OnTimeSetListener    {
+
+    @Override
+    public Dialog onCreateDialog(Bundle saveInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        return new TimePickerDialog(getActivity(), this, hour, minute, true);
+    }
+
+    public void onTimeSet(TimePicker view, int hour, int minute) {
+        //do something
+    }
+
+}
